0af156cb1834681c64e281028a04c0dc38f88424
silvioprog <silvioprog@gmail.com>
2018-09-18 11:46:39 -0300

    Bump v1.1.0

---
+++ search/defines_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sg_5ferr_5fsize',['SG_ERR_SIZE',['../sagui_8h.html#a05b92ec408039700f4786871eac486df',1,'sagui.h']]]
+];
--- search/searchdata.js
+++ search/searchdata.js
@@ -5,7 +5,8 @@ var indexSectionsWithContent =
   2: "es",
   3: "s",
   4: "s",
-  5: "ahsu"
+  5: "s",
+  6: "ahsu"
 };
 
 var indexSectionNames =
@@ -15,7 +16,8 @@ var indexSectionNames =
   2: "files",
   3: "functions",
   4: "typedefs",
-  5: "groups"
+  5: "defines",
+  6: "groups"
 };
 
 var indexSectionLabels =
@@ -25,6 +27,7 @@ var indexSectionLabels =
   2: "Files",
   3: "Functions",
   4: "Typedefs",
-  5: "Modules"
+  5: "Macros",
+  6: "Modules"
 };
 
